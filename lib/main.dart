9495046dcc2966837abed78df69d6b7a1cdf4401
ricardogomezv <183384@ids.upchiapas.edu.com>
2021-03-14 03:01:52 -0600

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_appc2/src/pages/Registro.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,7 +23,10 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      //MyHomePage(title: 'Flutter Demo Home Page'),
+     home: Center(
+      //child : Registro(),
+      ),
     );
   }
 }
---
+++ lib/src/pages/registro.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Registro extends StatelessWidget{
+String usuario = '';
+  String password = '';
+  bool mostrarPassword = false;
+
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      
+      body: Container(
+        decoration: new BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color(0x660b619c),
+                      Color(0x990b619c),
+                      Color(0xcc0b619c),
+                      Color(0xff0b619c),
+                    ]
+          )
+        ),
+
+        child: Column(
+          
+           mainAxisAlignment: MainAxisAlignment.center,
+
+          children: <Widget>[
+
+            SizedBox(height: 30,),
+
+            Text("Registro", style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+              fontWeight: FontWeight.w500,
+            ), 
+              textAlign: TextAlign.center,
+            ),
+
+            SizedBox(height: 10,),
+
+            TextField(
+              onChanged: (texto){
+                usuario = texto;
+              },
+
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(15.0)),
+                hintText: 'Ingrese Email',
+                icon: Icon(
+                  Icons.mail_outline,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+
+            SizedBox(height: 10,),
+
+            TextField(
+              onChanged: (texto){
+                password = texto;
+              },
+              
+              obscureText: !this.mostrarPassword,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                suffixIcon: IconButton(icon: Icon(
+                  Icons.remove_red_eye,
+                  color: this.mostrarPassword ? Colors.blue : Colors.grey,
+                ), 
+                
+                onPressed: (){this.mostrarPassword = !this.mostrarPassword;}
+                ),
+
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(15.0)),
+                hintText: 'Ingrese Contraseña',
+                icon: Icon(
+                  Icons.lock,
+                  color: Colors.white,
+                
+                ),
+              ),
+            ),
+
+            SizedBox(height: 10,),
+
+            TextField(
+              onChanged: (texto){
+                password = texto;
+              },
+              
+              obscureText: !this.mostrarPassword,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                suffixIcon: IconButton(icon: Icon(
+                  Icons.remove_red_eye,
+                  color: this.mostrarPassword ? Colors.blue : Colors.grey,
+                ), 
+                
+                onPressed: (){this.mostrarPassword = !this.mostrarPassword;}
+                ),
+
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(15.0)),
+                hintText: 'Confirme contraseña',
+                icon: Icon(
+                  Icons.lock,
+                  color: Colors.white,
+                
+                ),
+              ),
+            ),
+
+            SizedBox(height: 10,),
+
+            ElevatedButton(
+              
+            
+              onPressed:(){
+            
+                
+              },
+              child:Text(
+                'Iniciar sesión',
+                style: TextStyle(color: Colors.white,
+                ),
+              ),
+
+              style: ButtonStyle(
+
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                    (Set<MaterialState> states) {
+                        return Color.fromRGBO(
+                       11,97,156,1.0); 
+                      },
+                    ),
+                  
+                ),
+              
+            ),
+
+            SizedBox(height: 40,),
+            
+          ],  
+        ),
+
+      ),
+
+    );
+  }
+
+}
